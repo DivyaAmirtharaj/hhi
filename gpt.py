ad4770a68c2709109ba44d66415368aac483e46f
Divya <divya.amirtharaj@gmail.com>
2023-03-23 13:36:02 -0400

    gpt intro

---
+++ gpt.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#Import open AI OS and System Modules
+import openai
+import os
+openai.api_key = os.environ['api_key']
+messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+]
+while True:
+    message = input("You: ")
+    if message:
+        messages.append(
+                {"role": "user", "content": message},
+        )
+        chat_completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages
+        )
+    answer = chat_completion.choices[0].message.content
+    print(f"ChatGPT: {answer}")
+    messages.append({"role": "assistant", "content": answer})
