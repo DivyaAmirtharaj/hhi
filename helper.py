6789148a038d80e81a7cb023364c35060b1c99d1
Divya <divya.amirtharaj@gmail.com>
2023-04-03 11:02:08 -0400

    finished data processing

--- helper.py
+++ helper.py
@@ -1,16 +1,33 @@
 import docx2txt
 import glob
+import os
 
-directory = glob.glob('dedoose_data/*.docx')
+class Processing:
+    def __init__ (self):
+        self.dir_name = 'dedoose_data/'
+        self.dir = os.listdir(self.dir_name)
+        self.new_dir = 'dedoose_data/clean_dedoose_data/'
+    
+    def fileClean(self):
+        for file_name in self.dir:
+            if file_name.endswith('.txt'):
+                print(file_name)
+                os.remove(os.path.join(self.dir_name, file_name))
 
-for file_name in directory:
-    print(file_name)
 
-    with open(file_name, 'rb') as infile:
-        outfile = open(file_name[:-5]+'.txt', 'w', encoding='utf-8')
-        doc = docx2txt.process(infile)
+    def fileWrite(self):
+        directory = glob.glob('dedoose_data/*.docx')
+        for file_name in directory:
+            print(file_name)
+            with open(file_name, 'rb') as infile:
+                outfile_name = os.path.join(self.new_dir, file_name[len(self.dir_name):-5]+'.txt')
+                print(outfile_name)
+                outfile = open(outfile_name, 'w', encoding='utf-8')
+                doc = docx2txt.process(infile)
+                outfile.write(doc)
+            outfile.close()
+            infile.close()
 
-        outfile.write(doc)
-
-    outfile.close()
-    infile.close()
+data = Processing()
+#data.fileClean()
+data.fileWrite()
