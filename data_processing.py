d21dc2b0b14aef44d9565b063cb2c42d2d5aa382
Divya <divya.amirtharaj@gmail.com>
2023-04-03 10:16:17 -0400

    data cleaning and processing

---
+++ data_processing.py
@@ -0,0 +1,16 @@
+import docx2txt
+import glob
+
+directory = glob.glob('data/*.docx')
+
+for file_name in directory:
+    print(file_name)
+
+    with open(file_name, 'rb') as infile:
+        outfile = open(file_name[:-5]+'.txt', 'w', encoding='utf-8')
+        doc = docx2txt.process(infile)
+
+        outfile.write(doc)
+
+    outfile.close()
+    infile.close()
