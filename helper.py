9bf6b10d3cd89d0a9976d431e13a43d5cbcdb616
Divya <divya.amirtharaj@gmail.com>
2023-04-12 16:28:49 -0400

    batching done for api requests

--- helper.py
+++ helper.py
@@ -1,19 +1,20 @@
 import docx2txt
 import glob
 import os
+import re
 
 class Processing:
     def __init__ (self):
         self.dir_name = 'dedoose_data/'
         self.dir = os.listdir(self.dir_name)
         self.new_dir = 'dedoose_data/clean_dedoose_data/'
+        self.justice_dir = 'dedoose_data/e_justice/'
     
     def fileClean(self):
         for file_name in self.dir:
             if file_name.endswith('.txt'):
                 print(file_name)
                 os.remove(os.path.join(self.dir_name, file_name))
-
 
     def fileWrite(self):
         directory = glob.glob('dedoose_data/*.docx')
@@ -28,14 +29,27 @@ class Processing:
             outfile.close()
             infile.close()
 
-    def fileProcess(self):
-        directory = self.new_dir
+    def fileProcess(self, directory):
         for file_name in os.listdir(directory):
             f = open(self.new_dir+file_name, 'r')
             file_content = f.read()
-            print(file_content)
-
+            outfile_name = os.path.join(self.justice_dir, file_name)
+            file_content = re.sub(r'\n\s*\n', '\n', file_content)
+            match = re.search(r'JUSTICE AND ACCOUNTABILITY(.*)NARRATIVES AND MEMORIALIZATION', file_content, re.DOTALL)
+            if match:
+                output_text = match.group(1).strip()
+                with open(outfile_name, 'w', encoding='utf-8') as f:
+                    f.write(output_text)
+                    print(outfile_name)
+    
+    def fileRead(self):
+        f = open('dedoose_data/e_justice/001 ZY.txt', 'r')
+        for line in f.readlines():
+            print(line)
+   
+section_start = 'JUSTICE AND ACCOUNTABILITY'
+section_end = 'NARRATIVES AND MEMORIALIZATION'
 data = Processing()
-#data.fileClean()
+#data.fileProcess('dedoose_data/clean_dedoose_data/')
 #data.fileWrite()
-data.fileProcess()
+#data.fileRead()
--- main.py
+++ main.py
@@ -3,19 +3,51 @@ import openai
 import os
 
 openai.api_key = os.environ['api_key']
-messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
-]
-while True:
-    message = input("Prompt: ")
-    if message:
-        messages.append(
-                {"role": "user", "content": message},
-        )
-        chat_completion = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=messages
-        )
-    answer = chat_completion.choices[0].message.content
-    print(f"ChatGPT: {answer}")
-    messages.append({"role": "assistant", "content": answer})
+
+def user_prompt():
+        messages = [
+                {"role": "system", "content": "You are a helpful assistant."},
+        ]
+        while True:
+                message = input("Prompt: ")
+                if message:
+                        messages.append(
+                                {"role": "user", "content": message},
+                        )
+                        chat_completion = openai.ChatCompletion.create(
+                                model="gpt-3.5-turbo",
+                                messages=messages
+                        )
+                answer = chat_completion.choices[0].message.content
+                print(f"ChatGPT: {answer}")
+                messages.append({"role": "assistant", "content": answer})
+
+def batch_requests():
+        # Read text from file
+        with open('dedoose_data/e_justice/001 ZY.txt', 'r') as f:
+                text = f.read()
+
+       # Set up GPT request parameters
+        model = 'text-davinci-002'  # or another model ID
+        max_context_len = 4096      # maximum context length for the selected model
+        batch_size = 3             # number of batches to split the text into
+        batch_len = len(text) // batch_size + 1  # length of each batch
+
+        # Split the input text into batches
+        batches = [text[i:i+batch_len] for i in range(0, len(text), batch_len)]
+
+        # Call the GPT API for each batch and accumulate the results
+        generated_text = ''
+        for batch in batches:
+                prompt = f"Give me a summary of the following input:\n{batch}\n\nOutput:"
+                response = openai.Completion.create(
+                        engine=model,
+                        prompt=prompt,
+                        max_tokens=1000,  # or another max token count
+                        n=1,              # or another number of responses to generate
+                        stop=None         # or another stopping sequence
+                )
+                generated_text += response.choices[0].text.strip()
+
+        # Print the generated text
+        print(generated_text)
